2f96ddea6bcee14ed33cd2711946dcdc2f072175
MariaAcevedo11 <macevedos2@eafit.edu.co>
2025-08-11 19:10:13 -0500

    Cambio en el tipo de configuracion

--- Sesion/views.py
+++ Sesion/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    
+    template_name = "Sesion/home.html"
+
+class SesionPageView(TemplateView):
+
+    template_name = "Sesion/sesion.html"
+
+    
