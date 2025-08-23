ad8c84c3318b58f12daeb3b1cd6c1bfabbdb9715
MariaAcevedo11 <macevedos2@eafit.edu.co>
2025-08-22 20:38:38 -0500

    Home and filters ready with images, all models added to admin section

--- Sesion/admin.py
+++ Sesion/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Sesion, Usuario, Producto, Reserva, Reseña, Cupon
+
+#Para que se vean en admin y poder meter las imagenes 
+admin.site.register(Sesion)
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(Reserva)
+admin.site.register(Reseña)
+admin.site.register(Cupon)
